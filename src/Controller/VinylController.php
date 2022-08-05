a2a940aa790cbd25dc3501e40e3c5a40f3fcf8ff
pamorim <pjamorim91@gmail.com>
2022-08-05 14:11:19 +0000

    Perfect

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\UnicodeString;
+
+class VynilController
+{
+  #[Route('/')]
+  public function homepage(): Response
+  {
+      return new Response('Title: PB and Jams');
+  }
+
+  #[Route('/browse/{slug}')]
+  public function browse(string $slug = null): Response
+  {
+      if($slug){
+        $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+      }else{
+        $title = 'All Genres';
+      }
+
+      return new Response('Genre: '.$title);
+  }
+}
