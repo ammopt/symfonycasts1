636320f0411bd8e78a2c6f84c7dd03e9d04d30e3
pamorim <pjamorim91@gmail.com>
2022-08-05 15:29:13 +0000

    added some twig goodies

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,24 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Component\String\UnicodeString;
+use function Symfony\Component\String\u;
 
-class VynilController
+class VinylController extends AbstractController
 {
   #[Route('/')]
   public function homepage(): Response
   {
-      return new Response('Title: PB and Jams');
+    $tracks = [
+        ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+        ['song' => 'Waterfalls', 'artist' => 'TLC'],
+        ['song' => 'Creep', 'artist' => 'Radiohead'],
+        ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+        ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+        ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+    ];
+
+      return $this->render('vinyl/homepage.html.twig', [
+        'title' => 'PB & Jams',
+        'tracks' => $tracks
+      ]);
   }
 
   #[Route('/browse/{slug}')]
   public function browse(string $slug = null): Response
   {
       if($slug){
-        $title = 'tet';
-        //$title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
       }else{
         $title = 'All Genres';
       }
